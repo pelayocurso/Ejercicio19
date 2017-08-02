ebaa7f73f2627316e54892daf17909ae1ddbdae9
pelayo <pelayocurso@gmail.com>
2017-08-02 17:36:08 +0200

    Tutorial page 1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Ejercicio19';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
